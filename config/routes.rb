86d92a3da3f56bd7a54461012fa2bc96337411f2
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-14 21:17:23 +0000

    chore: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  Sidekiq::Web.use ActionDispatch::Cookies
+  Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
+  mount Sidekiq::Web => '/sidekiq'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "rails/health#show", as: :rails_health_check
+  match '*path', via: :all, to: proc { [404, {}, []] }
 end
