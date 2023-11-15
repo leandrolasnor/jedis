a39e2c735d9e334716fce7fb7196bdead97a3724
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-15 03:36:40 +0000

    chore: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   Sidekiq::Web.use ActionDispatch::Cookies
   Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
   mount Sidekiq::Web => '/sidekiq'
+
+  scope '/v1' do
+    resources :people, only: [:create]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
