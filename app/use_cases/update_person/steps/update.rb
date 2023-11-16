15c6e48649873a6f10a20b4ffcf2e294f84b33cd
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-16 17:59:52 +0000

    feat: dispatching person.status.updated event

--- app/use_cases/update_person/steps/update.rb
+++ app/use_cases/update_person/steps/update.rb
@@ -6,12 +6,15 @@ class UpdatePerson::Steps::Update
   extend  Dry::Initializer
 
   register_event 'person.updated'
+  register_event 'person.status.updated'
 
   def call(record:, **params)
     ApplicationRecord.transaction do
       record.with_lock do
         record.update!(params)
       end
+
+      publish('person.status.updated', status: record.status) if record.previous_changes.key?(:status)
       publish('person.updated', record: record)
       record
     end
