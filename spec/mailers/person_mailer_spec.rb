e542783d675dacf0891c02679b875c4264fcd8a4
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-17 15:44:43 +0000

    test: test case for welcome email message

--- spec/mailers/person_mailer_spec.rb
+++ spec/mailers/person_mailer_spec.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
-RSpec.describe PersonMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe PersonMailer do
+  context 'on welcome_email' do
+    let(:person) { create(:person) }
+    let(:mail) { described_class.welcome_email(person).deliver_now }
+
+    it 'must be able to send email' do
+      expect { mail }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      expect(mail.subject).to eq(I18n.t('mailer.person.welcome'))
+      expect(mail.to).to eq([person.email])
+      expect(mail.from).to eq(['jedis@challenge.com'])
+      expect(mail.body.encoded).to match(person.name)
+      expect(mail.body.encoded).to match(person.taxpayer_number)
+    end
+  end
 end
