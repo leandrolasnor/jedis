5ef22264d7ed7877cb724857b021198885ef1654
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-17 17:07:46 +0000

    refactor: if query param is nil then ''

--- app/use_cases/search_people/monad.rb
+++ app/use_cases/search_people/monad.rb
@@ -6,7 +6,7 @@ class SearchPeople::Monad
   extend  Dry::Initializer
 
   option :model, type: Interface(:page), default: -> { SearchPeople::Models::Person }, reader: :private
-  option :searcher, type: Instance(Proc), default: -> { proc { model.ms_raw_search(_1, limit: _2, offset: _3) } }, reader: :private
+  option :searcher, type: Instance(Proc), default: -> { proc { model.ms_raw_search(_1 || '', limit: _2, offset: _3) } }, reader: :private
 
   def call(query: '', page: 1, per_page: 5)
     Try { searcher.(query, per_page.to_i, (page.to_i - 1) * per_page.to_i)['hits'] }
