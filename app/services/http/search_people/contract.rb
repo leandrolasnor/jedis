0ff20ca5121e45fac2f4a8badea174b13cc91fe0
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-11-17 17:07:08 +0000

    fix: convert empty string to nil

--- app/services/http/search_people/contract.rb
+++ app/services/http/search_people/contract.rb
@@ -4,6 +4,6 @@ class Http::SearchPeople::Contract < ApplicationContract
   params do
     required(:page).filled(:integer)
     optional(:per_page).filled(:integer)
-    optional(:query).filled(:string)
+    optional(:query).maybe(:string)
   end
 end
